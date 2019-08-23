e5aa596b301da4d12bf448e9cf13552e1a0cc786
Rob Anthony <reanthony@usgs.gov>
2019-08-23 14:23:58 -0600

    Added location code to file names, added code to ensure time windows start on exact hours,added threshold for data completeness for each 1 hour window

--- PSD_Compression_Test.py
+++ PSD_Compression_Test.py
@@ -13,44 +13,53 @@ from scipy.signal import welch
 # "Global" variables
 client = Client("IRIS")
 debug = True
+time_run = True
 
 # Size of PSD Window in Seconds
 window = 3600
+Comp_Tres = 0.9
 
 nfft = 2**15
 windlap = 0.75
-time_run = True
 
-
-net, sta, chan = "IU", "ANMO", "BHZ"
+net, sta, chan = "US", "ECSD", "BHZ"
 
 ##################################### Psd calculation
 
 
-def write_results(net, sta, chan, ctime, power, freq):
-    if not os.path.exists(net + '_' + sta + '_' + chan + '_freqs.txt'):
+def write_results(net, sta, loc, chan, ctime, power, freq):
+    if not os.path.exists(net + '_' + sta + '_' + loc + '_' + chan + '_freqs.txt'):
         f = open(net + '_' + sta + '_' + chan + '_freqs.txt', 'w')
         for fr in freq:
             f.write(str(fr) + '\n')
         f.close()
-    fname = 'PSD_' + net + '_' + sta + '_' + chan + '_'
+    fname = 'PSD_' + net + '_' + sta + '_' + loc + '_' + chan + '_'
     fname += str(ctime.year) + '_' + str(ctime.julday).zfill(3)
     fname += str(ctime.hour).zfill(2)
+    if debug:
+        fname += str(ctime.minute).zfill(2)
     f = open(fname+ '.pckl', 'wb')
     pickle.dump(power, f)
     f.close()
     return
 
 
-def calc_psd(net, sta, chan, ctime, time_run = True):
+def calc_psd(net, sta, chan, ctime, time_run):
     if time:
         data_time = time.time()
+    estime = ctime + (24.*60.*60.) 
+    
     try:
-        st = client.get_waveforms(net, sta, "*", chan, ctime,
-                                  ctime + (24.*60.*60.), attach_response=True)
-        st.detrend('constant')
-        st.merge(fill_value=0.)
-
+        st = client.get_waveforms(net, sta, "*", chan, ctime, estime,
+                                   attach_response=True)
+        st.detrend('constant')                           
+        st.merge(fill_value=0.)    
+                    
+        # This should make sure we have 24 complete hours of data
+        st.trim(starttime=ctime, endtime=estime, pad=True, fill_value=0.)
+        if debug:
+            print(st) 
+        
     except:
         print('IRIS issue for: ' + net + ' ' + sta + str(ctime))
         return
@@ -59,35 +68,50 @@ def calc_psd(net, sta, chan, ctime, time_run = True):
         psd_time = time.time()
 
 
-    for stT in st.slide(60.*60, 60.*60.):
-        try:
-            if 'fs' not in vars():
+    for stT in st.slide(window, window):
+        
+        if 'fs' not in vars():
                 fs = 1./stT[0].stats.delta
-            # Default uses a Hann Window and demeans the data
-            freq, power = welch(stT[0].data, fs=fs, nperseg=nfft,
-                                noverlap=nfft*windlap, detrend='linear')
-            freq, power = freq[1:], power[1:]
-            # if this is a time sink we could change this
-
-            resp, freqR = stT[0].stats.response.get_evalresp_response(t_samp=1./fs,
-                                                                  nfft=nfft, output='ACC')
-            resp = resp[1:]
-            power = np.abs(power)
-            power = 10.*np.log10(power/(np.abs(resp)**2))
-        except:
-            print('Problem computing PSD' + stT[0].id())
+        
+        data_count = np.count_nonzero(stT[0].data)
+        total_count = window*fs
+        
+        if data_count/total_count >= Comp_Tres:
+            try:
+                # Default uses a Hann Window and demeans the data
+                freq, power = welch(stT[0].data, fs=fs, nperseg=nfft,
+                                    noverlap=nfft*windlap, detrend='linear')
+                freq, power = freq[1:], power[1:]
+                # if this is a time sink we could change this
+    
+                resp, freqR = stT[0].stats.response.get_evalresp_response(t_samp=1./fs,
+                                                                      nfft=nfft, output='ACC')
+                resp = resp[1:]
+                power = np.abs(power)
+                power = 10.*np.log10(power/(np.abs(resp)**2))
+                loc = stT[0].stats.location
+                write_results(net, sta, loc, chan, stT[0].stats.starttime, power, freq)
+                
+                
+            except:
+                print('Problem computing PSD' + stT[0].id())
+                continue
+        else:
+            print('Not enough data in this window:', (data_count/total_count)*100,
+            '% Complete. Skipping this Window.') 
             continue
-        if time:
-            print('It took', time.time() - psd_time , 's to calculate the PSDs')
-        write_results(net, sta, chan, stT[0].stats.starttime, power, freq)
-
+            
+    if time:
+        print('It took', time.time() - psd_time , 's to calculate the PSDs')
+        
     return
+        
 
 # Grab station start time and end time
 stime = UTCDateTime('1995-001T00:00:00')
 etime = UTCDateTime('2019-001T00:00:00')
 inv = client.get_stations(starttime=stime, endtime=etime, station=sta,
-                          channel="HH*", network=net, level="response")
+                          channel=chan, network=net, level="response")
 
 if debug:
     print(inv)
@@ -97,8 +121,8 @@ for netinv in inv:
         etime = stainv.end_date
 
 # Keep this in for testing and remove for actual run
-stime = UTCDateTime('2019-001T00:00:00')
-etime = UTCDateTime('2019-003T00:00:00')
+stime = UTCDateTime('2017-001T00:00:00')
+etime = UTCDateTime('2017-006T00:00:00')
 # Loop over the times for each PSD calculation
 ctime = stime
 
