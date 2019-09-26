4870a3db4ca2e921819ec5b64860807c511ee95c
aringler-usgs <aringler@usgs.gov>
2019-09-26 09:57:39 -0600

    I added the ability to do cross-power.

--- compute_psd.py
+++ compute_psd.py
@@ -7,15 +7,16 @@ import numpy as np
 from obspy.clients.fdsn import Client
 from obspy.core import UTCDateTime
 from scipy.signal import welch
+from matplotlib.mlab import csd
 
 # "Global" variables
 client = Client("IRIS")
 
-test_run = False
+test_run = True
 
-path = '/data/BHZ_TA/'
+path = '/data/test_case/'
 
-net, chan = "TA", "BHZ"
+net, chan1, chan2 = "US", "BHZ", "BH1"
 
 # Size of PSD Window in Seconds
 window = 3600
@@ -27,62 +28,71 @@ nfft = 2**10
 windlap = 0.75
 
 ##################################### Psd calculation
-def get_dataless(net, chan, stime, etime, client):
+def get_dataless(net, stime, etime, client):
     if os.path.exists(path + net + '_metadata.pickle'):
         with open(path + net + '_metadata.pickle', 'rb') as fhand:
             inv = pickle.load(fhand)
     else:
         client = Client()
         inv = client.get_stations(starttime=stime, endtime=etime, station="*",
-                                  channel=chan, network=net, level="response")
+                                  channel="*H*", network=net, level="response")                    
         with open(path + net + '_metadata.pickle', 'wb') as fhand:
             pickle.dump(inv, fhand)
     return inv
 
 
+def write_results(net, sta, loc, chan1, chan2, ctime, power, freq, phase=False):
+    if phase:
+        tag = 'PH'
+    else:
+        tag = 'PSD'
+    if not os.path.exists(path + sta + '_' + tag):
+        os.mkdir(path + sta + '_' + tag)
 
-def write_results(net, sta, loc, chan, ctime, power, freq):
-    if not os.path.exists(path + sta + '_PSD'):
-        os.mkdir(path + sta + '_PSD')
-
-    if not os.path.exists(path + sta + '_PSD/' + net + '_' + sta + '_' + loc + '_' + chan + '_freqs.txt'):
-        f = open(path + sta + '_PSD/' + net + '_' + sta + '_' + chan + '_freqs.txt', 'w')
+    if not os.path.exists(path + sta + '_'+ tag +'/' + net + '_' + sta + '_' + 
+                          loc + '_' + chan1 + '_' + chan2+ '_freqs.txt'):
+        f = open(path + sta + '_' + tag + '/' + net + '_' + 
+                 sta + '_' + chan1 + '_' + chan2 +  '_freqs.txt', 'w')
         for fr in freq:
             f.write(str(fr) + '\n')
         f.close()
-    fname = 'PSD_' + net + '_' + sta + '_' + loc + '_' + chan + '_'
+    fname = tag + '_' + net + '_' + sta + '_' + loc + '_' + chan1 + '_' + chan2 + '_'
     fname += str(ctime.year) + '_' + str(ctime.julday).zfill(3)
     fname += str(ctime.hour).zfill(2)
-    f = open(path + sta + '_PSD/' + fname+ '.pckl', 'wb')
+    f = open(path + sta + '_' + tag + '/' + fname+ '.pckl', 'wb')
     pickle.dump(power, f)
     f.close()
     return
     
     
-def psd_done(net, sta, loc, chan, ctime):
-    fname = 'PSD_' + net + '_' + sta + '_' + loc + '_' + chan + '_'
+def psd_done(net, sta, loc, chan1, chan2, ctime):
+    fname = 'PSD_' + net + '_' + sta + '_' + loc + '_' + chan1 + '_' + chan2 + '_'
     fname += str(ctime.year) + '_' + str(ctime.julday).zfill(3)
     fname += str(ctime.hour).zfill(2)
     print(fname)
     return os.path.exists(path + sta + '_PSD/' + fname + '.pckl')
     
-def data_done(net, sta, chan, ctime):
-    fname = 'PSD_' + net + '_' + sta + '_*_' + chan + '_'
+def data_done(net, sta, chan1, chan2, ctime):
+    fname = 'PSD_' + net + '_' + sta + '_*_' + chan1 + '_' + chan2 + '_'
     fname += str(ctime.year) + '_' + str(ctime.julday).zfill(3)
     files = glob.glob(path + sta + '_PSD/' + fname + '*.pckl')
     return len(files)
         
 
-def calc_psd(net, sta, chan, ctime, inv_sta, debug = False):
+def calc_psd(net, sta, chan1, chan2, ctime, inv_sta, debug = False):
     estime = ctime + (24.*60.*60.)
-    result_str =  str(ctime.julday) + ', ' + str(ctime.year) + ', ' + chan + '\n'
+    result_str =  str(ctime.julday) + ', ' + str(ctime.year) + ', ' + chan1 + ', ' + chan2 + '\n'
     try:
 
-        num_files = data_done(net, sta, chan, ctime)
+        num_files = data_done(net, sta, chan1, chan2, ctime)
         if num_files == 24:
             return 'Not grabbing'
-        st = client.get_waveforms(net, sta, "*", chan, ctime, estime,
+        st = client.get_waveforms(net, sta, "*", chan1, ctime, estime,
                                    attach_response=False)
+        
+        st += client.get_waveforms(net, sta, "*", chan2, ctime, estime,
+                                   attach_response=False)
+        
         st.detrend('constant')
         st.merge(fill_value=0.)
         # This should make sure we have 24 complete hours of data
@@ -93,30 +103,60 @@ def calc_psd(net, sta, chan, ctime, inv_sta, debug = False):
         return 'IRIS Problem, ' + result_str
     
     for stT in st.slide(window, window):
-        if psd_done(net, sta, stT[0].stats.location, chan, stT[0].stats.starttime):
+        if psd_done(net, sta, stT[0].stats.location, chan1, chan2, stT[0].stats.starttime):
             if debug:
-                print('Skipping:' + net + ' ' + sta + ' ' + stT[0].stats.location +  ' ' + chan)
+                print('Skipping:' + net + ' ' + sta + ' ' + stT[0].stats.location +  ' ' + chan1, + ' ' + chan2)
                 print(ctime)
             continue
         if 'fs' not in vars():
                 fs = 1./stT[0].stats.delta
-        data_count = np.count_nonzero(stT[0].data)
+        if chan1 == chan2:
+            data_count = np.count_nonzero(stT[0].data)
+        else:    
+            data_count1 = np.count_nonzero(stT[0].data)
+            data_count2 = np.count_nonzero(stT[1].data)
+            data_count = min([data_count1, data_count2])
         total_count = window*fs
         if data_count/total_count >= Comp_Tres:
             try:
                 # Default uses a Hann Window and demeans the data
-                freq, power = welch(stT[0].data, fs=fs, nperseg=nfft,
-                                    noverlap=nfft*windlap, detrend='linear')
-                freq, power = freq[1:], power[1:]
+                # Need to switch to cross-power
+                if chan1 == chan2:
+                    idx1, idx2 = 0, 0
+                else:
+                    idx1, idx2 = 0, 1
+                cxy, freq = csd(stT[idx1].data, stT[idx2].data, NFFT=nfft, 
+                                noverlap = nfft*windlap, Fs=fs,
+                                scale_by_freq=True, detrend='linear')
+                
+                # Be careful with cxy it is complex and not real
+                freq, cxy = freq[1:], cxy[1:]
                 # if this is a time sink we could change this
-                resp_inv = inv_sta.get_response(stT[0].id, stT[0].stats.starttime)
-                resp, freqR = resp_inv.get_evalresp_response(t_samp=1./fs,
-                                                             nfft=nfft, output='ACC')
-                resp = resp[1:]
-                power = np.abs(power)
-                power = 10.*np.log10(power/(np.abs(resp)**2))
+                resp_inv1 = inv_sta.get_response(stT[0].id, stT[0].stats.starttime)
                 loc = stT[0].stats.location
-                write_results(net, sta, loc, chan, stT[0].stats.starttime, power, freq)
+                if chan1 == chan2:
+                    resp, freqR = resp_inv1.get_evalresp_response(t_samp=1./fs,
+                                                             nfft=nfft, output='ACC')
+                    resp = resp[1:]
+                    power = np.abs(cxy)
+                    power = 10.*np.log10(power/(np.abs(resp)**2))
+                    
+                else:
+                    resp_inv2 = inv_sta.get_response(stT[1].id, stT[1].stats.starttime)
+                    resp1, freqR = resp_inv1.get_evalresp_response(t_samp=1./fs,
+                                                             nfft=nfft, output='ACC')
+                    resp2, freqR = resp_inv2.get_evalresp_response(t_samp=1./fs,
+                                                             nfft=nfft, output='ACC')
+                    resp1, resp2 = resp1[1:], resp2[1:]
+                    cxy /= ((np.abs(resp1)**2)*(np.abs(resp2)**2))
+                    cxy *=  resp2*np.conjugate(resp1)
+                    # Now we have the response removed
+                    power = 10.*np.log10(np.abs(cxy))
+                    phase = np.unwrap(np.angle(cxy))
+                    write_results(net, sta, loc, chan1, chan2, stT[0].stats.starttime, power, freq, phase=True)
+                write_results(net, sta, loc, chan1, chan2, stT[0].stats.starttime, power, freq)
+                    
+                
             except:
                 print('Problem computing PSD' + stT[0].id())
                 continue
@@ -136,10 +176,10 @@ etime = UTCDateTime('2019-001T00:00:00')
 if test_run:
     client = Client()
     inv = client.get_stations(starttime=stime, endtime=etime, station="*K",
-                              channel=chan, network=net, level="response")
+                              channel="*H*", network=net, level="response")
     print(inv)
 else:
-    inv = get_dataless(net, chan, stime, etime, client)
+    inv = get_dataless(net, stime, etime, client)
 
 
 
@@ -164,16 +204,19 @@ def run_station(net_sta):
         stime = UTCDateTime('2018-001T00:00:00')
         etime = stime + 356*24*60*60
     ctime = stime
+    if not os.path.exists(path):
+        os.mkdir(path)
     f = open(path + 'log_file_' + tsta, 'w')
     while ctime <= etime:
-        try:
-            info = calc_psd(tnet, tsta, chan, ctime, inv_sta)
+        #try:
+        if True:
+            info = calc_psd(tnet, tsta, chan1, chan2, ctime, inv_sta)
             if info == 'Not grabbing':
                 pass
             else:
                 f.write(info)
-        except:
-            f.write('Outerloop issue, ' + str(ctime.julday) + ', ' + str(ctime.year) + ', ' + chan + '\n')
+        #except:
+        #    f.write('Outerloop issue, ' + str(ctime.julday) + ', ' + str(ctime.year) + ', ' + chan1 + ', '  + chan2 + '\n')
         ctime += 24.*60.*60.
     f.close()
     return
